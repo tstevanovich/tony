35462a552e007f21d4d8e5ec8728eb8ed49072d7
Tony Stevanovich <tstevanovich@zenmonics.com>
2018-08-16 14:45:52 -0400

    prod build, mobile fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,21 @@ export class AppComponent implements OnInit {
     { link: 'skills', label: 'Skills' },
     { link: 'resume', label: 'Resume' }
   ];
+  socialLinks = [
+    {
+      link: 'https://www.facebook.com/stevanovich',
+      label: 'Facebook',
+      icon: 'fa fa-facebook-square'
+    },
+    { link: 'https://twitter.com/TonyStevanovich', label: 'Twitter', icon: 'fa fa-twitter-square' },
+    {
+      link: 'https://www.linkedin.com/in/tony-stevanovich-7030b11/',
+      label: 'LinkedIn',
+      icon: 'fa fa-linkedin-square'
+    },
+    { link: 'https://github.com/tstevanovich', label: 'Github', icon: 'fa fa-github-square' },
+    { link: 'mailto:tonys@yahoo.com', label: 'Email', icon: 'fa fa-envelope' }
+  ];
 
   constructor() {}
 
